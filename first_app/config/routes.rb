cecb7a1c4bdd166c3c784edcb2502eb451a80ca3
pilarMagda <106218860+pilarMagda@users.noreply.github.com>
2022-08-04 11:30:55 -0400

    incorporacion de navbar_2

--- first_app/config/routes.rb
+++ first_app/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#one'
+  # get 'pages/one'
+  get 'pages/two'
+  get 'pages/three'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
